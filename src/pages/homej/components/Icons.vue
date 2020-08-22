7d6822da47f89f05007ebaeeecd1bdeaba071f3c
JIANCL-cyber <1424371827@qq.com>
2020-08-22 11:18:12 +0800

    change

--- src/pages/homej/components/Icons.vue
+++ src/pages/homej/components/Icons.vue
@@ -21,7 +21,7 @@ export default {
       iconList: [{
         id: '0001',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-        desc: '景点门票s'
+        desc: '景点门票'
       }, {
         id: '0002',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
