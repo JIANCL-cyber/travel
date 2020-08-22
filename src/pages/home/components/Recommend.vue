3d6690a6b5a4dcd4301ff1b35d7d532d0a298d2d
JIANCL-cyber <1424371827@qq.com>
2020-08-22 15:47:50 +0800

    city header done

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -20,26 +20,6 @@ export default {
   props: {
     recommendList: Array
   }
-  // data () {
-  //   return {
-  //     recommendList: [{
-  //       id: '0001',
-  //       imgUrl: 'http://img1.qunarzz.com/sight/p0/2006/55/55f6406af2a258b2a3.water.jpg_200x200_87f21a01.jpg',
-  //       title: '清远周边逛',
-  //       desc: '人人都有一双发现美的眼睛，身边的事物最熟悉不过，那你可曾发现了被人遗漏的美？'
-  //     }, {
-  //       id: '0002',
-  //       imgUrl: 'http://img1.qunarzz.com/sight/p0/1807/92/9284d31ca59b10dda3.img.jpg_200x200_71ac1bdf.jpg',
-  //       title: '浮在海上的绿洲',
-  //       desc: '南国滨海之城，避寒胜地，有暖暖的阳光，海风习习，吃着海鲜逛逛老街多惬意啊'
-  //     }, {
-  //       id: '0003',
-  //       imgUrl: 'http://img1.qunarzz.com/sight/p0/1410/20/41d723daadc98045fe6b7283ba0af40c.jpg_200x200_5a05d5bc.jpg',
-  //       title: '阳江温泉知多少',
-  //       desc: '这里的天空和大海让人心旷神怡，配上舒缓的优美音乐，泡个暖暖的温泉岂不美哉'
-  //     }]
-  //   }
-  // }
 }
 </script>
 
